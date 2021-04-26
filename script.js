d4cb82fd95a1a007b70e05f64558938ee6357bb4
erolemre1 <emreglcfb@gmail.com>
2021-04-27 00:27:37 +0300

    added weather app

---
+++ script.js
@@ -0,0 +1,42 @@
+const url ="http://api.openweathermap.org/data/2.5/"
+const key ="c180726f39a5188e5414dcf8ec48a73f"
+
+const setQuery = (e) => {
+
+    if(e.keyCode == "13")
+    getResult(searchBar.value)
+}
+
+    const getResult =(cityName) => {
+        let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`
+        fetch(query)
+        .then(weather =>{
+            return weather.json()
+        })
+        .then(displayResult)
+}
+
+const displayResult = (result) => {
+  let city= document.querySelector(".city")
+  city.innerText= `${result.name},${result.sys.country}`
+ 
+  let temp = document.querySelector(".temp")
+  temp.innerText= `${Math.round(result.main.temp)}°C`
+  
+  let desc = document.querySelector(".desc")
+  desc.innerText= result.weather[0].description
+
+  let minmax = document.querySelector(".minmax")
+  minmax.innerText =`${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
+  
+  let feelslike = document.querySelector(".feelslike")
+  feelslike.innerText =`${Math.round (result.main.feels_like)}°C`
+ 
+  let humidity = document.querySelector(".humidity")
+  humidity.innerText =`${Math.round (result.main.humidity)}`
+ 
+}
+
+
+const searchBar= document.getElementById("searchBar")
+searchBar.addEventListener("keypress",setQuery)
